0a5447921145a9529715fb2ac7dabb245c2fb5be
Aheige <aheige17@gmail.com>
2023-11-07 14:26:11 +0100

    Color validity check on string. Button is disabled if not valid. Delete button on color and localstorage is updated accordingly.

--- src/components/Color/Color.tsx
+++ src/components/Color/Color.tsx
@@ -1,11 +1,14 @@
 import React, { FormEvent, useEffect } from 'react'
 import { useState } from 'react'
-import { AppBar, Button, Card, Chip, Stack, TextField } from '@mui/material'
+import { AppBar, Button, Card, Chip, InputBaseComponentProps, Stack, TextField, ThemeProvider, Typography } from '@mui/material'
 
+//Icons
 import colorIcon from '../../../public/colorIcon.svg'
+import DeleteIcon from '@mui/icons-material/Delete'
 
 //Styles
-import { addColor, chosenColors, iconStyle, nav } from './ColorStyle'
+import { addColor, chosenColors, iconStyle, nav, theme } from './ColorStyle'
+import './ColorCss.css'
 
 const Color = () => {
   const [colors, setColors] = useState<string[]>([])
@@ -37,28 +40,48 @@ const Color = () => {
   const handleAddColor = () => {
     if (colors && newColor.length > 0) {
       if (isValidColor(newColor)) {
-        console.log('handleAddColor')
         newColor && setColors([...colors, newColor])
         localStorage.setItem('colors', JSON.stringify([...colors, newColor]))
         setNewColor('')
-      } else {
-        alert(newColor + ' is not a color')
       }
     }
+  }
+
+  const deleteColor = (i: number) => {
+    colors.splice(i, 1)
+    setColors(colors.map((c) => c))
+    localStorage.setItem('colors', JSON.stringify(colors.map((c) => c)))
+  }
+
+  const inputProp: InputBaseComponentProps = {
+    style: {
+      color: isValidColor(newColor) ? '#fff' : 'rgb(130,130,130)',
+    },
   }
 
   return (
     <>
       <AppBar elevation={0} style={nav}>
         <Stack direction={'row'} spacing={3}>
-          <Card style={addColor}>
+          <Card style={addColor} elevation={0}>
             <form onSubmit={(e) => handleSubmit(e)}>
               <Stack direction={'row'} spacing={2}>
                 <div style={iconStyle}>
                   <img width={'25px'} src={colorIcon}></img>
                 </div>
-                <TextField value={newColor} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setNewColor(event.target.value)} label='#color' variant='filled' />
-                <Button color={'inherit'} variant='contained' onClick={() => handleAddColor()}>
+                <TextField
+                  InputLabelProps={{
+                    style: {
+                      color: 'rgb(130,130,130)',
+                    },
+                  }}
+                  inputProps={inputProp}
+                  style={{ color: '#fff' }}
+                  value={newColor}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => setNewColor(event.target.value)}
+                  label='#color'
+                />
+                <Button disabled={!isValidColor(newColor)} color={'success'} variant='contained' onClick={() => handleAddColor()}>
                   Add Color
                 </Button>
               </Stack>
@@ -67,22 +90,32 @@ const Color = () => {
         </Stack>
       </AppBar>
       <div style={chosenColors}>
-        {colors.length > 0 &&
-          colors.map((color, i) => {
-            const chosenColorCard: React.CSSProperties = {
-              padding: '2em',
-              background: color,
-            }
+        {colors.length > 0
+          ? colors.map((color, i) => {
+              const chosenColorCard: React.CSSProperties = {
+                padding: '3em',
+                background: color,
+              }
 
-            return (
-              <Card style={{ padding: '1em' }}>
-                <Stack direction={'column'} spacing={1}>
-                  <Chip avatar={<span style={{ background: color, borderRadius: '50%' }}></span>} label={color} />
-                  <Card elevation={10} style={chosenColorCard} key={i}></Card>
-                </Stack>
-              </Card>
-            )
-          })}
+              return (
+                <Card className='colorCard' key={i}>
+                  <ThemeProvider theme={theme}>
+                    <Button className='deleteBtn' onClick={() => deleteColor(i)}>
+                      <DeleteIcon />
+                    </Button>
+                  </ThemeProvider>
+                  <Stack direction={'column'} spacing={0.5}>
+                    <Chip avatar={<span style={{ background: color, borderRadius: '50%' }}></span>} label={color} />
+                    <Card elevation={10} style={chosenColorCard} key={i}></Card>
+                  </Stack>
+                </Card>
+              )
+            })
+          : [isValidColor(newColor) ? 'Its happening!' : 'No colors created yet :('].map((text, i) => (
+              <Typography key={i} style={{ color: 'rgb(130,130,130)' }} variant='h4'>
+                {text}
+              </Typography>
+            ))}
       </div>
     </>
   )
--- src/components/Color/ColorStyle.ts
+++ src/components/Color/ColorStyle.ts
@@ -1,10 +1,12 @@
+import { createTheme } from '@mui/material'
+
 export const addColor: React.CSSProperties = {
   display: 'flex',
   // gap: '1em',
   alignContent: 'center',
   justifyContent: 'center',
   padding: '1em',
-  // background: 'transparent',
+  background: 'transparent',
   width: 'fit-content',
 }
 
@@ -30,6 +32,25 @@ export const nav: React.CSSProperties = {
   display: 'flex',
   justifyContent: 'center',
   alignContent: 'center',
-  background: 'transparent',
+  background: '#414141',
   flexDirection: 'row',
 }
+
+export const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          display: 'none',
+          position: 'absolute',
+          transition: 'all 1s ease-in-out',
+          opacity: '0',
+          width: '2em',
+          height: '2em',
+          padding: '2em',
+          minWidth: 'unset',
+        },
+      },
+    },
+  },
+})
