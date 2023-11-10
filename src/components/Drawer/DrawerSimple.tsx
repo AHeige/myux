7fb36599e05c6b568821d5b092e160e97d25d2df
Aheige <aheige17@gmail.com>
2023-11-10 10:31:42 +0100

    Type issue icons

--- src/components/Drawer/DrawerSimple.tsx
+++ src/components/Drawer/DrawerSimple.tsx
@@ -2,7 +2,7 @@ import { Button, Drawer, Stack } from '@mui/material'
 
 //Icons
 import BorderStyleIcon from '@mui/icons-material/BorderStyle'
-import PaletteIcon from '@mui/icons-material/Palette'
+import { ColorLens } from '@mui/icons-material'
 import { useNavigate } from 'react-router-dom'
 
 const DrawerSimple = () => {
@@ -12,7 +12,7 @@ const DrawerSimple = () => {
     <Drawer open variant='persistent' elevation={0} PaperProps={{ style: { backgroundColor: '#414141' } }}>
       <Stack spacing={2} direction={'column'} sx={{ marginTop: '1em' }}>
         <Button onClick={() => navigate('/')}>
-          <PaletteIcon sx={{ color: '#fff' }} />
+          <ColorLens sx={{ color: '#fff' }} />
         </Button>
         <Button title='Design' onClick={() => navigate('/design')}>
           <BorderStyleIcon sx={{ color: '#fff' }} />
